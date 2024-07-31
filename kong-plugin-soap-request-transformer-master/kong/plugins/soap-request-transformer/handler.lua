7a95c64b33b952e37e8a1e6bf1be55aa2408e7ec
kamelia.ait-yahia.auditeur <kamelia.ait-yahia.auditeur@lecnam.net>
2024-07-31 19:12:49 +0200

    correct json response

--- kong-plugin-soap-request-transformer-master/kong/plugins/soap-request-transformer/handler.lua
+++ kong-plugin-soap-request-transformer-master/kong/plugins/soap-request-transformer/handler.lua
@@ -9,13 +9,29 @@ local xml2lua = require("xml2lua")
 local concat = table.concat
 local cjson = require("cjson")
 
+
 local CONTENT_TYPE = "content-type"
 local CONTENT_LENGTH = "content-length"
+
 
 local SoapTransformerHandler = {
     VERSION = "0.0.1",
     PRIORITY = 801,
 }
+local function remove_unwanted_elements(e, unwanted_attrs, unwanted_elements)
+    if type(e) == "table" then
+        for k, v in pairs(e) do
+            -- Supprimer les attributs non désirés
+            if unwanted_attrs[k] then
+                e[k] = nil
+            else
+                -- Nettoyer les sous-éléments
+                remove_unwanted_elements(v, unwanted_attrs, unwanted_elements)
+            end
+        end
+    end
+end
+
 
 local function remove_attr_tags(e)
     if type(e) == "table" then
@@ -28,27 +44,71 @@ local function remove_attr_tags(e)
     end
 end
 
+
+-- function SoapTransformerHandler.convertXMLtoJSON(xml, conf)
+--     local xmlHandler = handler:new()
+--     local parser = xml2lua.parser( xmlHandler )
+--     parser:parse(xml)
+--     local SOAPPrefix = "SOAP-ENV"
+
+
+--     if     string.match(xml,"soapenv:Envelope") then SOAPPrefix = "soapenv"
+--     elseif string.match(xml,"soap:Envelope"   ) then SOAPPrefix = "soap"
+--     elseif string.match(xml,"soap12:Envelope" ) then SOAPPrefix = "soap12" end
+
+
+--     local t = xmlHandler.root[SOAPPrefix .. ":Envelope"][SOAPPrefix .. ":Body"]
+--     if conf.remove_attr_tags then
+--         remove_attr_tags(t)
+--     end
+
+
+--     return cjson.encode(t)
+-- end
 function SoapTransformerHandler.convertXMLtoJSON(xml, conf)
     local xmlHandler = handler:new()
-    local parser = xml2lua.parser( xmlHandler )
+    local parser = xml2lua.parser(xmlHandler)
     parser:parse(xml)
     local SOAPPrefix = "SOAP-ENV"
 
-    if     string.match(xml,"soapenv:Envelope") then SOAPPrefix = "soapenv"
-    elseif string.match(xml,"soap:Envelope"   ) then SOAPPrefix = "soap" 
-    elseif string.match(xml,"soap12:Envelope" ) then SOAPPrefix = "soap12" end
+
+    if string.match(xml, "soapenv:Envelope") then SOAPPrefix = "soapenv"
+    elseif string.match(xml, "soap:Envelope") then SOAPPrefix = "soap"
+    elseif string.match(xml, "soap12:Envelope") then SOAPPrefix = "soap12" end
+
 
     local t = xmlHandler.root[SOAPPrefix .. ":Envelope"][SOAPPrefix .. ":Body"]
+
+
     if conf.remove_attr_tags then
         remove_attr_tags(t)
     end
 
+
+    -- Définir les attributs et éléments non désirés à supprimer
+    local unwanted_attrs = {
+        ["xsi:nil"] = true,
+        ["xsi:type"] = true
+    }
+   
+    local unwanted_elements = {
+        -- Liste des éléments à supprimer si nécessaire
+    }
+   
+    -- Nettoyer les éléments non désirés
+    remove_unwanted_elements(t, unwanted_attrs, unwanted_elements)
+
+
     return cjson.encode(t)
 end
+
+
+
 
 function SoapTransformerHandler:access(conf)
     local body = get_raw_body()
     local is_body_transformed, body = access.transform_body(conf, body,get_header(CONTENT_TYPE))
+
 
     if is_body_transformed then
         set_raw_body(body)
@@ -56,19 +116,24 @@ function SoapTransformerHandler:access(conf)
         set_header(CONTENT_TYPE, "text/xml;charset=UTF-8")
     end
 
+
 end
+
 
 function SoapTransformerHandler:header_filter(conf)
     kong.response.clear_header("Content-Length")
     kong.response.set_header("Content-Type", "application/json")
 end
 
+
 function SoapTransformerHandler:body_filter(conf)
     local ctx = ngx.ctx
     local chunk, eof = ngx.arg[1], ngx.arg[2]
 
+
     ctx.rt_body_chunks = ctx.rt_body_chunks or {}
     ctx.rt_body_chunk_number = ctx.rt_body_chunk_number or 1
+
 
     -- if eof wasn't received keep buffering
     if not eof then
@@ -78,17 +143,21 @@ function SoapTransformerHandler:body_filter(conf)
         return
     end
 
+
     -- if bad gateway status recieved return
     if kong.response.get_status() == 502 then
         return nil
     end
 
+
     -- last piece of body is ready
     local resp_body = concat(ctx.rt_body_chunks)
+
 
     if not resp_body or resp_body == '' then
         return nil
     end
+
 
     kong.log.debug("Response body XML: "..resp_body)
     ngx.arg[1] = self.convertXMLtoJSON(resp_body, conf)
@@ -96,4 +165,9 @@ function SoapTransformerHandler:body_filter(conf)
 end
 
 
+
+
 return SoapTransformerHandler
+
+
+
